b04c7ba604d6dec744466386a75d72e81ac292e8
Izuchukwu Kevin <kevinfizu@gmail.com>
2019-12-01 23:16:05 +0100

    Trying to fix api subdomain

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
 
     protected function mapApiRoutes(): void
     {
-    	Route::domain(env('API_URL'))
+    	Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
