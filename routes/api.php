593bfbc737cfcb820b4ab67f786f94fc180aa720
Izuchukwu Kevin <kevinfizu@gmail.com>
2019-12-03 06:49:14 +0100

    Oops, missed api.php file in previous commit

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Route;
 
-Route::get('profile', 'API\\v1\\AuthController@profile');
-Route::post('login', 'API\\v1\\AuthController@login');
-Route::post('register', 'API\\v1\\AuthController@register');
+Route::get('/', 'Api\\v1\\AuthController@index');
+Route::post('password', 'Api\\v1\\AuthController@password');
+Route::post('login', 'Api\\v1\\AuthController@login');
+Route::post('register', 'Api\\v1\\AuthController@register');
+Route::post('logout', 'Api\\v1\\AuthController@logout');
+
+Route::post('users/{id}/profile', 'Api\\v1\\StoresController@profile');
+Route::post('stores/{id}/logo', 'Api\\v1\\StoresController@logo');
+
 Route::apiResources([
 	'categories' => 'Api\\v1\\CategoriesController',
 	'orders' => 'Api\\v1\\OrdersController',
