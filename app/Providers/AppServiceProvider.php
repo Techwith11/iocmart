4de55c6d3164f24066d4355692571f853f9c3d50
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-02-24 02:16:17 +0100

    Reverting public path back to normal

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
 	{
-		$this->app->bind('path.public', static function(){
-            return env('APP_ENV') === 'production' ? DIRECTORY_SEPARATOR.'public' : base_path().DIRECTORY_SEPARATOR.'public';
-        });
+		//
     }
 }
